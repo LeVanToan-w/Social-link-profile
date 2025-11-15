73eba89116cbd271822885e441faa8b498156066
LeVanToan-w <tl7378132@gmail.com>
2025-11-15 07:34:00 -0500

    update

---
+++ script.js
@@ -0,0 +1,16 @@
+function face() {
+    window.location.href = "#";
+}
+
+function zalo() {
+    window.location.href = "#";
+}
+function dis() {
+    window.location.href = "#";
+}
+function sms() {
+    window.location.href = "#";
+}
+function email() {
+    window.location.href = "#";
+}
